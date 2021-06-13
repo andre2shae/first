12c6c870f1340045bbfe48dd8579a090f8890717
andre <andre2shae@gmail.com>
2021-06-13 21:05:46 +0100

    first

--- first.py
+++ first.py
@@ -1,4 +1,20 @@
-from random import randint
+def multiply(num, num2):
+    return num * num2
 
-randomnumber = randint(1, 10)
-print(randomnumber)
+re = "y"
+
+while re != "n":
+    num = input("Please type a number: ")
+    while not num.isnumeric():
+        print("That's not a number!")
+        num = input("Please type a number: ")
+    num2 = input("Please type another number: ")
+    while not num2.isnumeric():
+        print("That's not a number!")
+        num2 = input("Please type another number: ")
+    result = multiply(int(num), int(num2))
+    print(f"{num} x {num2} = {result}")
+    re = input("Would you like to do another multiplication? (y/n) ")
+    if re == "n":
+        print("Thank you for playing!")
+        break
